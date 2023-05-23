52c8ecef19e96d37a86b23576efeb4b6afdb023d
Bush28 <sharnaliyazmin@gmail.com>
2023-05-23 14:19:18 -0400

    update

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code here!
+const removeNode = document.getElementById("main").remove();
+const newHeader = document.createElement('h1')
+newHeader.setAttribute('id','victory')
+newHeader.textContent=('BUSHRA is the champion')
